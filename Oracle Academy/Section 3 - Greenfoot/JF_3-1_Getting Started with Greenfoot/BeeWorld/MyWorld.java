85dbd386f410ee9b960b0132b33f58d10d50ec67
Sudiartika90402 <114633936+Sudiartika90402@users.noreply.github.com>
2022-11-13 08:41:31 +0700

    Add files via upload

---
+++ Oracle Academy/Section 3 - Greenfoot/JF_3-1_Getting Started with Greenfoot/BeeWorld/MyWorld.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Bee bee = new Bee();
+        addObject(bee,274,170);
+        Fly fly = new Fly();
+        addObject(fly,395,72);
+        Fly fly2 = new Fly();
+        addObject(fly2,120,287);
+        Fly fly3 = new Fly();
+        addObject(fly3,439,205);
+        Bee bee2 = new Bee();
+        addObject(bee2,105,86);
+    }
+}
